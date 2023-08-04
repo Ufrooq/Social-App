04f8b8b47e2ee2dbe983f58dcc0a8175fd084250
Umar <muhammadshafqatali3@gmail.com>
2023-08-05 00:07:26 +0500

    sdfsd

--- server/Controllers/auth.js
+++ server/Controllers/auth.js
@@ -3,3 +3,39 @@ import jwt from "jsonwebtoken";
 import { UserModel } from "../Models/user";
 
 // REGISTERING THE USER
+
+const registerUser = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile,
+      impressions,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await UserModel.create({
+      firstName,
+      lastName,
+      hashPassword,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.lo("Error  ", error);
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/Models/user.js
+++ server/Models/user.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema(
       required: true,
       min: 20,
     },
-    lastName: {
+    picturePath: {
       type: String,
       default: "",
     },
