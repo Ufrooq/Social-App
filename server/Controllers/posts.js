2dc9986a2a305ef7627b40ea291139e00ef3c03b
Umar <muhammadshafqatali3@gmail.com>
2023-09-18 12:04:17 +0500

    working on posts controllers

--- server/Controllers/posts.js
+++ server/Controllers/posts.js
@@ -16,11 +16,48 @@ export const createPost = async (req, res) => {
       comments: [],
     });
     const updatedPosts = await postsModel.find();
-    res.status(200).json(updatedPosts);
+    res.status(201).json(updatedPosts);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
-export const getFeedPosts = async (req, res) => {};
-export const getUserPosts = async (req, res) => {};
-export const likePost = async (req, res) => {};
+export const getFeedPosts = async (req, res) => {
+  try {
+    const feedPosts = await postsModel.find();
+    res.status(200).json(feedPosts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const userPosts = await postsModel.find({ userId });
+    res.status(200).json(userPosts);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const likePost = async (req, res) => {
+  try {
+    const { postId } = req.params; //that's the id of post to be liked or like removed
+    const { userId } = req.body; //that's the id of user who is going like or like remove
+    const post = await postsModel.find(postId);
+    const isLiked = post.likes.get(userId);
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+    const updatePost = await postsModel.findByIdAndUpdate(
+      postId,
+      {
+        likes: post.likes,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatePost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
