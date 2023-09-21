913f99d0cde04b1ece14ab5d7d037d9180afd8b0
Umar <muhammadshafqatali3@gmail.com>
2023-09-22 00:16:16 +0500

    adds-component

--- front-end/src/Components/Add.js
+++ front-end/src/Components/Add.js
@@ -1,16 +1,25 @@
 import React from "react";
 import add_media from "../assets/product.jpg";
+import "./styles/add.scss";
 
 const Add = () => {
   return (
-    <div className="card">
+    <div className="add_card">
       <div className="main">
-        <span>Sponsered</span>
+        <p>Sponsered</p>
         <span>Create Ad</span>
       </div>
       <div className="media">
-        <img src={add_media} alt="" srcset="" />
+        <img src={add_media} alt="" />
       </div>
+      <div className="details">
+        <p>Mika Cosmetics</p>
+        <span>mikacosmetics.com</span>
+      </div>
+      <p id="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quia
+        sads?
+      </p>
     </div>
   );
 };
--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from "../Components/Profile";
 import "./styles/style.scss";
+import Add from "../Components/Add";
 
 const Home = () => {
   return (
     <div className="home-page">
       <Profile />
+      <Add />
     </div>
   );
 };
