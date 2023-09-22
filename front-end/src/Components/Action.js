d5335a1db2ff9921b8211b6830d2748be1cfac10
Umar <muhammadshafqatali3@gmail.com>
2023-09-22 14:28:25 +0500

    action-card

--- front-end/src/Components/Action.js
+++ front-end/src/Components/Action.js
@@ -6,8 +6,26 @@ const Action = () => {
     <div className="action-card">
       <div className="my-mind">
         <img src={profile_image} alt="" />
+        <input type="text" />
       </div>
-      <div className="actions"></div>
+      <div className="actions">
+        <div>
+          <i class="fa-solid fa-image"></i>
+          image
+        </div>
+        <div>
+          <i class="fa-solid fa-video"></i>
+          clip
+        </div>
+        <div>
+          <i class="fa-solid fa-paperclip"></i>
+          Attachment
+        </div>
+        <div>
+          <i class="fa-solid fa-microphone"></i>
+          audio
+        </div>
+      </div>
     </div>
   );
 };
