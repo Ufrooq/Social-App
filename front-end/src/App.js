ecef2f8654dfb3d86a343c5b5f06f5c4c90fe18a
Umar <muhammadshafqatali3@gmail.com>
2023-09-26 14:52:07 +0500

    front-end-part

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
+import Login from "./Components/Login";
 import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Home />
+      {/* <NavBar />
+      <Home /> */}
+      <Login />
     </div>
   );
 }
--- front-end/src/Components/Login.js
+++ front-end/src/Components/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "./styles/login.scss";
+import illustration_image from "../assets/undraw_secure_login_pdn4.svg";
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <div className="login_page">
+      <div className="login_form">
+        <form>
+          <input type="email" required />
+          <input type="password" required />
+        </form>
+      </div>
+      <div className="illustration">
+        <img src={illustration_image} alt="" srcset="" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
